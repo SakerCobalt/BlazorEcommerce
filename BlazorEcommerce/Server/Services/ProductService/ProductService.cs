cd5ff8dfb78444e93dc5bd2a1edcb43fe361c30e
SakerCobalt <weston@ohiogray.org>
2022-09-11 07:04:59 -0400

    Add project files.

---
+++ BlazorEcommerce/Server/Services/ProductService/ProductService.cs
@@ -0,0 +1,19 @@
+﻿namespace BlazorEcommerce.Server.Services.ProductService
+{
+    public class ProductService : IProductService
+    {
+        private readonly DataContext context;
+        public ProductService(DataContext context)
+        {
+            this.context = context;
+        }
+        public async Task<ServiceResponse<List<Product>>> GetProductListAsync()
+        {
+            var response = new ServiceResponse<List<Product>>
+            {
+                Data = await context.Products.ToListAsync()
+            };
+            return response;
+        }
+    }
+}
